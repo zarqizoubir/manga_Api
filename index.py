6c05a32d224ed22230802e4118bbfe1f3048c31d
ezzoubair <ezzoubair@pop-os.localdomain>
2022-12-10 00:51:52 +0100

    remove handlers option f=at this moment

--- index.py
+++ index.py
@@ -1,16 +1,30 @@
 import requests
 
-end = "http://127.0.0.1:8000/token/"
+end = "http://127.0.0.1:8000/mangas/create/"
 
+file1 = open("db.sqlite3", "rb")
+file2 = open(".gitignore", "rb")
 
 data = {
-    "username": "someone",
-    "password": "",
+    "data":
+    [
+        {
+            "name": "naruto",
+        },
+        {
+            "name": "fullmetall",
+
+        },
+    ]
 }
 
 res = requests.post(
     end,
-    data=data
+    data=data,
+    files={
+        "poster": file1,
+        "poster": file2,
+    }
 )
 
 print(res.json())
