b95680e365bb08f4026649c8cde6fa8b416e82c3
ezzoubair <ezzoubair@pop-os.localdomain>
2022-12-11 02:20:23 +0100

    [#30] (add Generics.ListAPIView instead of APIview)for listing data processing

--- manga/urls.py
+++ manga/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.MangaCreateListApiView.as_view()),
+    path("test/", views.MangaGenericListCreateApiView.as_view()),
     path("<str:manga>/", views.MangaRetrieveUpdateDestroy.as_view()),
     path("<str:manga>/chapters/", views.ChapterCreateListApiView.as_view()),
     path("<str:manga>/chapters/<str:number>/",
--- manga/views.py
+++ manga/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 
 from rest_framework import status
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.request import HttpRequest
 from rest_framework.response import Response
@@ -10,6 +11,41 @@ from rest_framework.response import Response
 from . import models
 from . import serializers
 
+
+# Start Migrating from APIView To genericsView
+
+class MangaGenericListCreateApiView(generics.ListCreateAPIView):
+    queryset = models.Manga.objects.all()
+    serializer_class = serializers.MangaSerializer
+
+    def get_search_query(self, params):
+        return self.get_queryset().filter(
+            Q(id_name__icontains=params) |
+            Q(name__icontains=params) |
+            Q(description__icontains=params)
+        ).all()
+
+    # def get(self, request, *args, **kwargs):
+    #     params = request.query_params["search"] if request.GET.get(
+    #         "search") != None else ""
+    #     if params:
+    #         serializer = serializers.MangaSearchSerializer(
+    #             self.get_search_query(params), many=True)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     serializer = self.serializer(self.get_objects(), many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        params = request.query_params["search"] if request.GET.get(
+            "search") != None else ""
+        if params:
+            queryset = self.get_search_query(params)
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().list(request, *args, **kwargs)
+
+
+# Standard Api View
 
 class MangaCreateListApiView(APIView):
     model = models.Manga
@@ -26,7 +62,6 @@ class MangaCreateListApiView(APIView):
         ).all()
 
     def get(self, request: HttpRequest):
-        # print(request.build_absolute_uri())
         params = request.query_params["search"] if request.GET.get(
             "search") != None else ""
 
