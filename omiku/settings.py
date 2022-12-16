df591a3cc0644318a765319058b83c4e913093f7
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2022-12-16 04:28:03 +0100

    pagination

--- omiku/settings.py
+++ omiku/settings.py
@@ -172,5 +172,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_THROTTLE_RATES': {
         'user': '10000/day'
-    }
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
